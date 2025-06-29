d537599f7ac801838b39fa2c33b526bf50f1fa73
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-06-29 17:26:00 -0300

    feat: primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fiap-tc-shell';
+  items = [
+    {
+      label: 'Angular',
+      icon: 'pi pi-palette',
+      route: '/angular',
+    },
+    {
+      label: 'Angular 2',
+      icon: 'pi pi-palette',
+      route: '/angular/home2',
+    },
+    {
+      label: 'React',
+      icon: 'pi pi-palette',
+      route: '/react',
+    },
+  ];
 }
