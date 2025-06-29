796653e3677e7b452542353c6bcd5d80b022720d
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-06-29 15:53:27 -0300

    feat: configuração angular remote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'angular',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './routes',
+      }).then((m) => m.routes),
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
+  name: "fiap-tc-shell",
 
-  name: 'fiap-tc-shell',
-
-  exposes: {
-    './Component': './src/app/app.component.ts',
+  remotes: {
+    "fiap-tc-angular": "fiap-tc-angular@http://localhost:4201/remoteEntry.js",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
-
 });
