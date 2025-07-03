be0de302a7b7dadf3b72a1516683e5d74345cb42
João Vitor Veronese Vieira <joao.vieira@nursa.com>
2025-07-03 08:20:55 -0300

    feat(shell): create menubar component

---
+++ src/app/core/menubar/menubar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './menubar.component.html',
+  imports: [MenubarModule],
+})
+export class MenubarComponent {
+
+}
