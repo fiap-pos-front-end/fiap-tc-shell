8dbd9d54d543583f24f6cce6d32605a66b4db923
João Vitor Veronese Vieira <joao.vieira@nursa.com>
2025-07-03 08:42:23 -0300

    feat(core): simplify menu with primeng

--- src/app/core/menubar/menubar.component.ts
+++ src/app/core/menubar/menubar.component.ts
@@ -10,20 +10,27 @@ import { MenubarModule } from 'primeng/menubar';
 })
 export class MenubarComponent {
   readonly menus: MenuItem[] = [
+    { label: 'Início' },
+    { label: 'Categorias' },
+    { label: 'Transferências' },
+    { label: 'Investimentos' },
     {
-      label: 'Angular',
-      icon: 'pi pi-palette',
-      route: '/angular',
-    },
-    {
-      label: 'Angular 2',
-      icon: 'pi pi-palette',
-      route: '/angular/home2',
-    },
-    {
-      label: 'React',
-      icon: 'pi pi-palette',
-      route: '/react',
+      label: 'Rotas',
+      icon: 'pi pi-map',
+      items: [
+        {
+          label: 'Angular',
+          routerLink: '/angular',
+        },
+        {
+          label: 'Angular 2',
+          routerLink: '/angular/home2',
+        },
+        {
+          label: 'React',
+          routerLink: '/react',
+        },
+      ],
     },
   ];
 }
