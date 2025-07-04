6e3cfd8b1be5861d665be7162f1f4a1a56ccacd8
João Vitor Veronese Vieira <joao.vieira@nursa.com>
2025-07-04 08:59:38 -0300

    feat: create basic page layout (structure)

--- src/app/core/menubar/menubar.component.ts
+++ src/app/core/menubar/menubar.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { Ripple } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
-  imports: [MenubarModule, RouterModule],
+  imports: [MenubarModule, AvatarModule, Ripple, CommonModule, RouterModule],
 })
 export class MenubarComponent {
   readonly menus: MenuItem[] = [
