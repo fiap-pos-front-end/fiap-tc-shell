4a567bdfef31494a4007e9cd010712a8a3e13854
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-12 20:13:46 -0300

    fix: httpclient export

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,14 @@ import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { provideHttpClient } from '@angular/common/http';
 
 registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
