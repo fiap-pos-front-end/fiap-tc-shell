6ea883a3315d0b9fa1b6a37e1f16ba3aaf5e080f
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-13 00:07:04 -0300

    refactor: replace /angular by /transferencias route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'angular',
+    path: 'transferencias',
     loadChildren: () =>
       loadRemoteModule({
         type: 'module',
--- src/app/core/menubar/menubar.component.ts
+++ src/app/core/menubar/menubar.component.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
-import { MenubarModule } from 'primeng/menubar';
 import { AvatarModule } from 'primeng/avatar';
+import { MenubarModule } from 'primeng/menubar';
 import { Ripple } from 'primeng/ripple';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menubar',
@@ -15,7 +15,7 @@ export class MenubarComponent {
   readonly menus: MenuItem[] = [
     { label: 'Início', routerLink: '' },
     { label: 'Categorias' },
-    { label: 'Transferências', routerLink: '/angular' },
+    { label: 'Transferências', routerLink: '/transferencias' },
     { label: 'Investimentos' },
   ];
 }
