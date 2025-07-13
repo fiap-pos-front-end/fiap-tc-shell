824852ed52c09570598721ce351e01307b11041d
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-12 22:02:30 -0300

    feat: temp input token

--- src/app/core/menubar/menubar.component.ts
+++ src/app/core/menubar/menubar.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, effect, model } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { AvatarModule } from 'primeng/avatar';
 import { Ripple } from 'primeng/ripple';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { emitEvent } from '@fiap-pos-front-end/fiap-tc-shared';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
-  imports: [MenubarModule, AvatarModule, Ripple, CommonModule, RouterModule],
+  imports: [
+    MenubarModule,
+    AvatarModule,
+    Ripple,
+    CommonModule,
+    RouterModule,
+    // TODO: REMOVER
+    InputTextModule,
+    FloatLabel,
+    FormsModule,
+    // FIM TODO
+  ],
 })
 export class MenubarComponent {
   readonly menus: MenuItem[] = [
@@ -18,4 +33,11 @@ export class MenubarComponent {
     { label: 'Transferências', routerLink: '/transacoes' },
     { label: 'Investimentos' },
   ];
+  // TODO: REMOVER
+  jwtToken = model('');
+  jwtTokenEffect = effect(() => {
+    const token = this.jwtToken();
+    emitEvent('jwtToken', token);
+  });
+  // FIM TODO
 }
