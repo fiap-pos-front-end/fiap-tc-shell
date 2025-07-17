c42b822a18f30f91660f2e06a67cd0f98ba860fa
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-16 23:35:04 -0300

    feat: create register flow

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { Toast } from 'primeng/toast';
-import { LoginService } from '../../shared/services/login/login.service';
+import { AuthResponsePayload, LoginService } from '../../shared/services/login/login.service';
 import { AuthStore } from '../../shared/store/auth/auth.store';
 
 @Component({
@@ -53,57 +53,35 @@ export class LoginComponent {
     }
 
     this.loginService.authUser(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: (res) => {
-        const token = res?.access_token;
-        if (token) {
-          this.authStore.setToken(token);
-          this.router.navigate(['/home']);
-          this.vps.scrollToPosition([0, 0]);
-        }
-      },
-      error: () => {
+      next: (res) => this.handleAuthResponse(res),
+      error: () =>
         this.messageService.add({
           severity: 'error',
           summary: 'Erro',
           detail: 'Não foi possível autenticar o usuário!',
           life: 2500,
-        });
-      },
+        }),
     });
   }
 
   createUser() {
-    if (this.registerForm.valid) {
-      const user = this.registerForm.value;
-      if (this.loginService.emailValidator(user.email)) {
-        this.loginService.createUser({ Body: user }).subscribe({
-          next: (res) => {
-            const token = res?.result?.token;
-            if (token) {
-              this.store.dispatch(setToken({ token }));
-            }
-            window.location.href = '/home';
-            this.vps.scrollToPosition([0, 0]);
-          },
-          error: () => {
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Erro',
-              detail: 'Não foi possível criar o usuário',
-              life: 2500,
-            });
-          },
-        });
-      } else {
-        this.messageService.add({
-          severity: 'warn',
-          summary: 'Atenção',
-          detail: 'Insira um e-mail válido',
-          life: 3000,
-        });
-      }
-    } else {
+    if (!this.registerForm.valid) {
       this.messageService.add({ severity: 'warn', summary: 'Atenção', detail: 'Preencha todos os campos', life: 2500 });
+      return;
+    }
+
+    const user = this.registerForm.value;
+    if (this.loginService.emailValidator(user.email)) {
+      this.loginService.createUser(user.username, user.email, user.password).subscribe({
+        next: (res) => this.handleAuthResponse(res),
+        error: () =>
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Não foi possível criar o usuário',
+            life: 2500,
+          }),
+      });
     }
   }
 
@@ -113,5 +91,14 @@ export class LoginComponent {
 
   closeLogin() {
     this.context.closeLogin();
+  }
+
+  private handleAuthResponse(res: AuthResponsePayload) {
+    const token = res?.access_token;
+    if (token) {
+      this.authStore.setToken(token);
+      this.router.navigate(['/home']);
+      this.vps.scrollToPosition([0, 0]);
+    }
   }
 }
--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -4,11 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 
 // TODO: mover isso para o shared
-type CreateUserResponsePayload = {
-  access_token: string;
-};
-
-type AuthUserResponsePayload = {
+export type AuthResponsePayload = {
   access_token: string;
 };
 
@@ -20,14 +16,18 @@ export class LoginService {
 
   private readonly userBaseUrl = `${environment.apiUrl}/auth`;
 
-  createUser(user: { Body: { username: string; email: string; password: string } }): Observable<any> {
-    return this.httpClient.post<CreateUserResponsePayload>(this.userBaseUrl, user.Body, {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    });
+  createUser(username: string, email: string, password: string): Observable<AuthResponsePayload> {
+    return this.httpClient.post<AuthResponsePayload>(
+      `${this.userBaseUrl}/register`,
+      { email, password },
+      {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      },
+    );
   }
 
-  authUser(email: string, password: string): Observable<AuthUserResponsePayload> {
-    return this.httpClient.post<AuthUserResponsePayload>(
+  authUser(email: string, password: string): Observable<AuthResponsePayload> {
+    return this.httpClient.post<AuthResponsePayload>(
       `${this.userBaseUrl}/login`,
       { email, password },
       {
