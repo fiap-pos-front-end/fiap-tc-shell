139be8e2acd4349dc8cd11fdd51663cfe0d5b79f
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-16 23:05:22 -0300

    feat: create logout

--- src/app/core/layout/menubar/menubar.component.ts
+++ src/app/core/layout/menubar/menubar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, viewChild } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { AvatarModule } from 'primeng/avatar';
 import { Menu, MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
 import { Ripple } from 'primeng/ripple';
+import { AuthStore } from '../../../shared/store/auth/auth.store';
 
 @Component({
   selector: 'app-menubar',
@@ -20,10 +21,21 @@ import { Ripple } from 'primeng/ripple';
   `,
 })
 export class MenubarComponent {
+  private readonly router = inject(Router);
+  private readonly authStore = inject(AuthStore);
+
+  readonly logout = viewChild<Menu>('logout');
+
   readonly logoutMenu: MenuItem[] = [{ label: 'Sair', routerLink: '/', icon: 'pi pi-sign-out' }];
+
   readonly menus: MenuItem[] = [
     { label: 'Início', routerLink: '/home', icon: 'pi pi-home', routerLinkActiveOptions: { exact: true } },
     { label: 'Categorias', routerLink: '/categorias', icon: 'pi pi-tags' },
     { label: 'Transferências', routerLink: '/transferencias', icon: 'pi pi-money-bill' },
   ];
+
+  onLogout() {
+    this.authStore.clearToken();
+    this.router.navigate(['/']);
+  }
 }
