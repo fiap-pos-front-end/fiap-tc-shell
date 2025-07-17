4d7a0bec6ff4946da5ff67e2ceb19f21706a5b3e
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-16 22:58:06 -0300

    feat: send token again on reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MenubarComponent } from '@shell/core';
-import { FooterComponent } from '@shell/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { FooterComponent, MenubarComponent } from '@shell/core';
+import { filter } from 'rxjs/operators';
+import { AuthStore } from './shared/store/auth/auth.store';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MenubarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'fiap-tc-shell';
+export class AppComponent implements OnInit {
+  private readonly authStore = inject(AuthStore);
+
+  isAuthenticated = signal(false);
+
   mainClass = '';
   secondClass = '';
-  isAuthenticated = true;
 
   constructor(private router: Router) {
     this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
@@ -23,5 +24,11 @@ export class AppComponent {
       this.mainClass = url == '/' ? 'flex-1 bg-slate-100' : 'flex-1 p-4 bg-slate-100';
       this.secondClass = url == '/' ? 'mx-auto max-w-10xl' : 'mx-auto max-w-7xl';
     });
+  }
+
+  ngOnInit(): void {
+    const token = this.authStore.token();
+    this.isAuthenticated.set(token !== '');
+    this.authStore.setToken(token); // TODO: talvez não seja o melhor jeito, mas por ora, garante que os MFEs receberão o novo token
   }
 }
--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -3,34 +3,42 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 
+// TODO: mover isso para o shared
+type CreateUserResponsePayload = {
+  access_token: string;
+};
+
+type AuthUserResponsePayload = {
+  access_token: string;
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   private readonly httpClient = inject(HttpClient);
 
-  private readonly userBaseUrl = `${environment.apiUrl}/user`;
+  private readonly userBaseUrl = `${environment.apiUrl}/auth`;
 
-  authUser(user: { Body: { email: string; password: string } }): Observable<any> {
-    return this.httpClient.post(`${this.userBaseUrl}/auth`, user.Body, {
+  createUser(user: { Body: { username: string; email: string; password: string } }): Observable<any> {
+    return this.httpClient.post<CreateUserResponsePayload>(this.userBaseUrl, user.Body, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     });
+  }
+
+  authUser(email: string, password: string): Observable<AuthUserResponsePayload> {
+    return this.httpClient.post<AuthUserResponsePayload>(
+      `${this.userBaseUrl}/login`,
+      { email, password },
+      {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      },
+    );
   }
 
   getUser(): Observable<any> {
     return this.httpClient.get(this.userBaseUrl, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     });
-  }
-
-  createUser(user: { Body: { username: string; email: string; password: string } }): Observable<any> {
-    return this.httpClient.post(this.userBaseUrl, user.Body, {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    });
-  }
-
-  emailValidator(email: string): boolean {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email.trim());
   }
 }
