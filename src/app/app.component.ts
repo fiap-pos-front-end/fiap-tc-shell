7b9442dc6bbdf02fd2542d1b25afbf614ca1c76a
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-16 23:29:06 -0300

    fix: improve reactivity after login/logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent, MenubarComponent } from '@shell/core';
 import { filter } from 'rxjs/operators';
@@ -11,6 +11,7 @@ import { AuthStore } from './shared/store/auth/auth.store';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  private readonly router = inject(Router);
   private readonly authStore = inject(AuthStore);
 
   isAuthenticated = signal(false);
@@ -18,17 +19,21 @@ export class AppComponent implements OnInit {
   mainClass = '';
   secondClass = '';
 
-  constructor(private router: Router) {
+  constructor() {
     this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
       const url = event.urlAfterRedirects;
       this.mainClass = url == '/' ? 'flex-1 bg-slate-100' : 'flex-1 p-4 bg-slate-100';
       this.secondClass = url == '/' ? 'mx-auto max-w-10xl' : 'mx-auto max-w-7xl';
     });
+
+    effect(() => {
+      const token = this.authStore.token();
+      this.isAuthenticated.set(!!token && token !== '');
+    });
   }
 
   ngOnInit(): void {
     const token = this.authStore.token();
-    this.isAuthenticated.set(token !== '');
-    this.authStore.setToken(token); // TODO: talvez não seja o melhor jeito, mas por ora, garante que os MFEs receberão o novo token
+    this.authStore.setToken(token); // Initialize token for MFEs
   }
 }
