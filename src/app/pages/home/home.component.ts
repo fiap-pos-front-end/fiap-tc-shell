163952a278512b08ed23634f1f7fdcd0f959bd4d
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-19 15:30:48 -0300

    feat: use real transaction for Extract and Resume

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,16 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { getLastEvent } from '@fiap-pos-front-end/fiap-tc-shared';
 import { ReactWrapperComponent } from '@shell/core';
-import { onEvent } from '@fiap-pos-front-end/fiap-tc-shared';
 import { StatementComponent } from './statement/statement.component';
+
+export type Transaction = {
+  id: number;
+  type: string;
+  amount: number;
+  category: string;
+  date: Date;
+};
 
 @Component({
   selector: 'app-home',
@@ -14,9 +22,35 @@ export class HomeComponent implements OnInit {
   user = 'Maria';
   dateToday = new Date();
   toggleVisibility = true;
-  balance = 0;
 
-  ngOnInit(): void {
-    onEvent('balanceChange', (val) => (this.balance = val));
+  balance = signal(0);
+  transactions = signal<Transaction[]>([]);
+
+  ngOnInit() {
+    const transactionsReceived = getLastEvent('transactions-updated');
+    this.transactions.set(this.mapTransactionsToViewModel(transactionsReceived));
+    this.balance.set(this.calculateBalance(this.transactions()));
+  }
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  private mapTransactionsToViewModel(transactions: any[]): Transaction[] {
+    return (transactions || []).map((transaction) => ({
+      amount: transaction.amount.amount,
+      category: transaction.category,
+      date: new Date(transaction.date),
+      id: parseInt(transaction.id),
+      type: transaction.type,
+    }));
+  }
+
+  private calculateBalance(transactions: Transaction[]): number {
+    // TODO: melhorar a tipagem quando estiver no /shared
+    return transactions.reduce((acc, transaction) => {
+      if (transaction.type === 'Receita') {
+        return acc + transaction.amount;
+      }
+
+      return acc - transaction.amount;
+    }, 0);
   }
 }
--- src/app/pages/home/statement/statement.component.ts
+++ src/app/pages/home/statement/statement.component.ts
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { DataViewModule } from 'primeng/dataview';
 import { TagModule } from 'primeng/tag';
+import { Transaction } from '../home.component';
 
 @Component({
   selector: 'app-statement',
@@ -12,11 +12,5 @@ import { TagModule } from 'primeng/tag';
   styleUrls: ['./statement.component.scss'],
 })
 export class StatementComponent {
-  transactions = signal<any>([
-    { id: 1, type: 'Receita', value: 100, category: 'Salário', date: new Date(2025, 6, 13) },
-    { id: 2, type: 'Despesa', value: 200, category: 'Alimentação', date: new Date(2025, 6, 13) },
-    { id: 3, type: 'Receita', value: 300, category: 'Proventos', date: new Date(2025, 6, 13) },
-    { id: 3, type: 'Receita', value: 300, category: 'Proventos', date: new Date(2025, 6, 13) },
-    { id: 3, type: 'Receita', value: 300, category: 'Proventos', date: new Date(2025, 6, 13) },
-  ]);
+  transactions = input.required<Transaction[]>();
 }
