c6ddbd445ed0a14c0462f8e5230cea8e3a288c42
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-22 20:42:33 -0300

    feat: removido desnecessarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CommonModule, DatePipe } from '@angular/common';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { EVENTS, TransactionDTO, emitEvent } from '@fiap-pos-front-end/fiap-tc-shared';
+import { Component, OnInit, signal } from '@angular/core';
+import { onEvent } from '@fiap-pos-front-end/fiap-tc-shared';
 import { ReactWrapperComponent } from '@shell/core';
-import { finalize } from 'rxjs';
-import { TransactionService } from '../../shared/services/transaction/transaction.service';
 import { StatementWrapperComponent } from '../../core/statement-wrapper/statement-wrapper.component';
 
 @Component({
@@ -13,41 +10,17 @@ import { StatementWrapperComponent } from '../../core/statement-wrapper/statemen
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  private readonly transactionService = inject(TransactionService);
+  // TODO: alterar nome do usuario, (split no email)
   user = 'Maria';
   dateToday = new Date();
   toggleVisibility = true;
   isLoading = true;
   balance = signal(0);
-  transactions = signal([]);
 
   ngOnInit() {
     this.initialLoading();
   }
   initialLoading() {
-    this.getAllTransactions();
-  }
-
-  private getAllTransactions() {
-    this.transactionService
-      .getAll()
-      .pipe(finalize(() => (this.isLoading = false)))
-      .subscribe((transactions: any) => {
-        const order = transactions.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
-
-        emitEvent(EVENTS.TRANSACTIONS_UPDATED, order);
-        this.transactions.set(order);
-        this.balance.set(this.calculateBalance(this.transactions()));
-      });
-  }
-
-  private calculateBalance(transactions: TransactionDTO[]): number {
-    return transactions.reduce((acc, transaction) => {
-      if (transaction.type === 'RECEITA') {
-        return acc + transaction.amount;
-      }
-
-      return acc - transaction.amount;
-    }, 0);
+    onEvent('totalBalanceChanged', (balance) => this.balance.set(balance));
   }
 }
