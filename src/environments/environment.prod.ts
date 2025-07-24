49c2f2460a34b62c5036032f188f740fa8e3755f
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-24 09:34:53 -0300

    feat: url prod api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://need-to-be-updated', // TODO: atualizar assim que fizermos o deploy na cloud
+  apiUrl: 'https://fiap-tc-api-production.up.railway.app',
   urlMfAngular: 'https://main.dfb4k5gye6ghy.amplifyapp.com/remoteEntry.js',
   urlMfAngular2: 'https://main.d2365zv6s1p7nn.amplifyapp.com/remoteEntry.js',
   urlMfReact: 'https://main.d197zuuniam3wp.amplifyapp.com/remoteEntry.js',
