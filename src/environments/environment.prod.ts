4dbb563dd5ef2e201f6021f8a723deb3da72ed94
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-28 16:27:05 -0300

    fix: url react

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://fiap-tc-api-production.up.railway.app',
   urlMfAngular: 'https://main.dfb4k5gye6ghy.amplifyapp.com/remoteEntry.js',
   urlMfAngular2: 'https://main.d2365zv6s1p7nn.amplifyapp.com/remoteEntry.js',
-  urlMfReact: 'https://feat-next.d3jvidrrs1cqk3.amplifyapp.com/_next/static/chunks/remoteEntry.js',
+  urlMfReact: 'https://main.d2n9ckiy7iu9yr.amplifyapp.com/_next/static/chunks/remoteEntry.js',
 };
